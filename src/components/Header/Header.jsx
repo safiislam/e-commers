8dcc56c9e955efcb1802a9506481cbc8264aac17
safiislam <118913244+safiislam@users.noreply.github.com>
2023-03-27 04:16:57 +0600

    new

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo  from '../../images/Logo.svg'
+
+const Header = () => {
+    return (
+        <nav className=' bg-[#1C2B35] h-[80px] px-10 flex justify-between items-center  '>
+            <img src={logo} alt="" />
+            <div className='text-white flex gap-6'>
+                <a href="/shop">Shop</a>
+                <a href="/order">Orders</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
